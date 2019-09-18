95edb6c765432bfefa085c86e394a18153c26bf2
Experimental account { Hendrik Schatz } <e18514@u7pc7.nscl.msu.edu>
2019-09-18 18:01:54 -0400

    new changes to debug and merge optimizer and GP code

--- optimizer.py
+++ optimizer.py
@@ -1,50 +1,60 @@
 import numpy as np
-from setup import SaveIm, SetCorr, GetCorr, SetQuads, GetQuads, GetBeamPos, Dist
+from time import *
+from setup import SaveIm, SetCorr, GetCorr, SetQuads, GetQuads, GetBeamPos, Dist, GaussProc, cont
 
+while (cont == 'yes'):
+	
+	#import current state
+	#Get initial corrector values
+	h13_init, v13_init, h31_init, v31_init= GetCorr()
+	#Get initial quad values
+	q1_init, q2_init, q3_init, q4_init= GetQuads()
 
-###import current state
-#Get initial corrector values
-h13_init, v13_init, h31_init, v31_init= GetCorr()
-#Get initial quad values
-q1_init, q2_init, q3_init, q4_init= GetQuads()
+	#Tuning Q1 and Q2
 
+	#take picture with all at init values
+	SetQuads(q1_init, q2_init, 0, 0)
+	sleep(7)
+	all_nom_im= SaveIm('allNom')
 
-###Tuning Q1 and Q2
+	#take picture with all at zero
+	SetQuads(0, 0, 0, 0)
+	sleep(7) #might need to increase this if the jumps in current are big
+	all_zero_im= SaveIm('allZero')
 
-##take pictures
-#take picture with all at init values
-SetQuads(q1_init, q2_init, 0, 0)
-all_nom_im= SaveIm('allNom')
+	#take picture with Q1 at half
+	SetQuads(q1_init/2, q2_init, 0, 0)
+	sleep(7)
+	q1_half_im= SaveIm('q1half')
 
-#take picture with all at zero
-SetQuads(0, 0, 0, 0)
-all_zero_im= SaveIm('allZero')
+	#take picture with Q2 at half
+	SetQuads(q1_init, q2_init/2, 0, 0)
+	sleep(7)
+	q2_half_im= SaveIm('q2half')
 
-#take picture with Q1 at half
-SetQuads(q1_init/2, q2_init, 0, 0)
-q1_half_im= SaveIm('q1half')
+	#return quads to original values
+	SetQuads(q1_init, q2_init, q3_init, q4_init)
 
-#take picture with Q2 at half
-SetQuads(q1_init, q2_init/2, 0, 0)
-q2_half_im= SaveIm('q2half')
- 
-#return quads to original values
-SetQuads(q1_init, q2_init, q3_init, q4_init)
+	#get centroids from pictures
+	pos_1= GetBeamPos(all_nom_im)
+	pos_2= GetBeamPos(all_zero_im)
+	pos_3= GetBeamPos(q1_half_im)
+	pos_4= GetBeamPos(q2_half_im)
 
-##get centroids from pictures
-pos_1= GetBeamPos(all_nom_im)
-pos_2= GetBeamPos(all_zero_im)
-pos_3= GetBeamPos(q1_half_im)
-pos_4= GetBeamPos(q2_half_im)
+	#get quadratic distance from centroids
+	print(pos_1, pos_2, pos_3, pos_4)
+	distance= Dist(pos_1, pos_2, pos_3, pos_4)
+	print("Dist= ", distance)
 
-##get quadratic distance from centroids
-distance= Dist(pos_1, pos_2, pos_3, pos_4)
+	#save corrector values and distance to file
+	f= open("correctorValues_Distance.txt", "a+")
+	c1, c2, c3, c4= GetCorr()
+	f.write(f'{c1} {c2} {c3} {c4} {distance}\n')
+	f.close()
 
-###Run GP thing here??#####
+	#run GP script
+	eps_input= input("Enter the desired acquisition function parameter:")
+	GaussProc(float(eps_input))
 
-
-#save corrector values and distance to file
-f= open("optimizer_output", "a+")
-c1, c2, c3, c4= GetCorr()
-f.write(f"{c1} {c2} {c3} {c4} {distance}\n")
-f.close()
+	#continue or not
+	cont= input("Continue? yes/no  ")
--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #Setup of PVs and defined functions for optimizer
 import datetime
 from time import *
-from os import system
+import os
 import numpy as np
 from epics import caget, caput, cainfo
+
+cont = 'yes'
 
 ##Set currents##
 q1_cset= 'SCR_BTS35:PSQ_D1475:I_CSET'
@@ -33,91 +35,98 @@ write_image= 'SCR_BTS35:VD_D1542:TIFF1:WriteFile'
 
 def GetQuads():
 
-	'''Gets current quad values'''
-	q1= caget(q1_ird, wait= True)
-	q2= caget(q2_ird, wait= True)
-	q3= caget(q3_ird, wait= True)
-	q4= caget(q4_ird, wait= True)
-	return q1, q2, q3, q4
+    '''Gets current quad values'''
+    q1= caget(q1_ird)
+    q2= caget(q2_ird)
+    q3= caget(q3_ird)
+    q4= caget(q4_ird)
+    return q1, q2, q3, q4
 
 
 def SetQuads(v1, v2, v3, v4):
 
-	'''Set quads to new value'''
+    '''Set quads to new value'''
 
-	caput(q1_cset, v1, wait= True)
-	caput(q2_cset, v2, wait= True)
-	caput(q3_cset, v3, wait= True)
-	caput(q4_cset, v4, wait= True)
-	print("Quads set.")
+    caput(q1_cset, v1, wait= True)
+    caput(q2_cset, v2, wait= True)
+    caput(q3_cset, v3, wait= True)
+    caput(q4_cset, v4, wait= True)
+    print("Quads set.")
 
 
 def GetCorr():
 
-	'''Gets the current corrector values'''
+    '''Gets the current corrector values'''
 
-	c1= caget(h13_ird, wait= True)
-	c2= caget(v13_ird, wait= True)
-	c3= caget(h31_ird, wait= True)
-	c4= caget(v31_ird, wait= True)
-	return c1, c2, c3, c4
+    c1= caget(h13_ird)
+    c2= caget(v13_ird)
+    c3= caget(h31_ird)
+    c4= caget(v31_ird)
+    return c1, c2, c3, c4
 
 
 def SetCorr(v1, v2, v3, v4):
 
-	'''Sets the correctors to new values'''
+    '''Sets the correctors to new values'''
 
-	caput(h13_cset, v1, wait= True)
-	caput(v13_cset, v2, wait= True)
-	caput(h31_cset, v3, wait= True)
-	caput(v31_cset, v4, wait= True)
-	print("Correctors set.")
+    caput(h13_cset, v1, wait= True)
+    caput(v13_cset, v2, wait= True)
+    caput(h31_cset, v3, wait= True)
+    caput(v31_cset, v4, wait= True)
+    print("Correctors set.")
 
 
 def SaveIm(tunename):
-
-	'''Save viewer image at FP1'''
+    
+    '''Save viewer image at FP1'''
 
     timestring = (datetime.datetime.now()).strftime("%m-%d_%H:%M.%f")
     filename='D1542'+'_'+tunename+'_'+timestring
     caput(set_image_name, filename, wait=True)   #sets image name
     caput(write_image, 1, wait=True)   #saves image
- 	print(f'Screenshot {tunename} obtained')
-	return filename
+    print(f"Screenshot {tunename} obtained")
+    return filename
 
 def GetBeamPos(imname):
 
-	'''Run viewer image analysis code to get centroids'''
+    '''Run viewer image analysis code to get centroids'''
 
-	#run viewer code
-	os.system(f"python3 ~/Document/Viewer-Image-Analysis/src/im_analysis {imname}.tiff")
-	#import text with results
-	data= np.loadtxt=(f"~/Document/Viewer-Image-Analysis/output/optimizer/BeamLoc_{imname}.csv")
-	#read x and y in mm
-	x_centroid= data[4]
-	y_centroid= data[5]
-	#return (x, y)
-	return x_centroid, y_centroid 
+    #run viewer code
+    os.system(f"python3 /user/e18514/Documents/Viewer-Image-Analysis/src/im_analysis.py /mnt/daqtesting/secar_camera/new_captures/{imname}_000.tiff")
+    #import text with results
+    data= np.loadtxt(f"/user/e18514/Documents/Viewer-Image-Analysis/output/optimizer/BeamLoc_{imname}_000.csv")
+    #read x and y in mm
+    x_centroid= data[4]
+    y_centroid= data[5]
+    #return (x, y)
+    return x_centroid, y_centroid 
 
 def Dist(p1, p2, p3, p4):
 
-	'''Get quadratic distance between the 4 centroids (mm)'''
+    '''Get quadratic distance between the 4 centroids (mm)'''
     meas_num= 12
-	x1, y1, x2, y2, x3, y3, x4, y4= p1[0], p1[1], p2[0], p2[1], p3[0], p3[1], p4[0], p4[1]
+    x1, y1, x2, y2, x3, y3, x4, y4= p1[0], p1[1], p2[0], p2[1], p3[0], p3[1], p4[0], p4[1]
     dist= 1/meas_num * (np.power((x1-x2),2)
                       + np.power((y1-y2),2)
                       + np.power((x1-x3),2) 
                       + np.power((y1-y3),2) 
                       + np.power((x1-x4),2) 
                       + np.power((y1-y4),2)
-					  + np.power((x2-x4),2) 
+		              + np.power((x2-x4),2) 
                       + np.power((y2-y4),2)
-					  + np.power((x3-x4),2) 
+		              + np.power((x3-x4),2) 
                       + np.power((y3-y4),2)
-					  + np.power((x2-x3),2) 
+		              + np.power((x2-x3),2) 
                       + np.power((y2-y3),2)
                        ) 
-    return np.sqrt(dist)
+    return np.sqrt(dist)*(-1)
+    
+def GaussProc(eps_input):
+
+    '''Run Gaussian Process script and set correctors to new value'''
+    
+    os.system(f"python /user/e18514/Documents/TuneOptimizer/GaussianProcess.py {eps_input}")
+    
 
 
 
